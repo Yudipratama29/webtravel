c7067bb46fdf363c718caade7910227d7392c2f9
Yudipratama29 <p.yudi2001@gmail.com>
2023-11-21 17:00:38 +0800

    Create Button Home and Back

--- resources/views/transportation/show.blade.php
+++ resources/views/transportation/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.master')
 
-@section('title', 'Show Transportation')
+@section('title', 'Details Transportation')
 
 @section('breadcrumb')
     <li class="breadcrumb-item"><a href="{{ url('/transportation') }}">Transportation</a></li>
@@ -9,15 +9,28 @@
 
 @section('content')
     <div class="card">
+        <style>
+            .card{
+                border: 1px;
+                padding: 20px;
+                margin: 20px;
+                border-radius: 8px;
+            }
+
+        </style>
         <div class="card-header">
             <div class="row">
                 <h4 class="card-title">Details Transportation</h4>
             </div>
         </div>
-        <div>
+        <div class="card-body">
             <p class="card-text">ID                  : {{$datatransportation->idtrans}}</p>
             <p class="card-text">Transportation      : {{$datatransportation->transportation}}</p>
             <p class="card-text">Driver              : {{$datatransportation->driver}}</p>
+        </div>
+        <div class="card-body">
+            <a class="btn btn-sm btn-danger" href="{{ url('/transportation/') }}">Back</a>
+            <a class="btn btn-sm btn-success" href="{{ url('/')}}">Home</a>
         </div>
     </div>
 @endsection
