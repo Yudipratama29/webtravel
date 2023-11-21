33bb55908f26ae8975d39450727396818ebd16b8
Yudipratama29 <p.yudi2001@gmail.com>
2023-11-21 17:00:57 +0800

    Modify Header

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,23 +1,23 @@
-<header class="d-flex flex-wrap justify-content-right py-1 mb-5 border-bottom">
-    <nav class="navbar bg-body-tertiary">
+<header class="navbar navbar-expand-lg bg-body-tertiary py-1 mb-5 border-bottom">
+    <nav class="navbar bg-body">
         <div class="container-fluid">
-          <a class="navbar-brans" href="/">
-            <img src="https://github.com/Yudipratama29/webtravel/blob/main/Traveloke%20Logo.png?raw=true" alt="Logo" width="50" height="50" class="d-inline-block">
+          <a class="" href="/">
+            <img src="https://github.com/Yudipratama29/webtravel/blob/main/Traveloke%20Logo.png?raw=true" alt="Logo" width="50" height="50" class="">
           </a>
           <a>
             Traveloke
           </a>
-          <ul class="nav nav-pills">
-            @php
-                $menu = [['url' => '/', 'name' => 'Home'], ['url' => 'destination', 'name' => 'Destination List'], ['url' => 'transportation', 'name' => 'Transportation'], ['url' => 'book', 'name' => 'Book']];
-            @endphp
-    
-            @foreach ($menu as $m)
-                @include('layout.nav-item', ['menu' => $m])
-            @endforeach
-        </ul>
+            <ul class="nav nav-underline justify-content-end">
+                <li class="nav-item">
+                    @php
+                        $menu = [['url' => '/', 'name' => 'Home'], ['url' => 'destination', 'name' => 'Destination List'], ['url' => 'transportation', 'name' => 'Transportation'], ['url' => 'book', 'name' => 'Book']];
+                    @endphp
+            
+                    @foreach ($menu as $m)
+                        @include('layout.nav-item', ['menu' => $m])
+                    @endforeach
+                </li>
+            </ul>
         </div>
-      </nav>
-
-    
+      </nav>  
 </header>
